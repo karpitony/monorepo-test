cb7a7779473b6d924334c7421ec3929dd495cdc0
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-11 14:36:00 +0900

    feat: Union 섹션 기초 틀 잡기

--- src/pages/Main/Union/Union.tsx
+++ src/pages/Main/Union/Union.tsx
@@ -1,7 +1,35 @@
+import styled from "styled-components";
+import ContentBox from "./ContentBox";
+
 export default function Union() {
   return (
-    <div>
-      <h2>Union Section</h2>
-    </div>
+    <section>
+      <Container>
+        <UnionText>Union</UnionText>
+        <ContentBox />
+      </Container>
+    </section>
   );
 }
+
+const Container = styled.section`
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center; 
+  height: 1500px;
+`
+
+const UnionText = styled.h2`
+  width: 132px;
+  height: 57px;
+
+  text-align: center; 
+  color: var(--FarmSystem_Orange);
+  font-size: 48px;
+  font-weight: 700; 
+
+  margin-top: 70px;
+  margin-bottom: 50px;
+`
+
