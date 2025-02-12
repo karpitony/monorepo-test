4eb34b2e064842d0f4b010aea10c2ff9fc3f2794
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-12 20:54:55 +0900

    feat: 트랙 선택 버튼 + 버튼 모음집 추가

--- src/pages/Main/Tracks/Tracks.styled.ts
+++ src/pages/Main/Tracks/Tracks.styled.ts
@@ -70,3 +70,45 @@ export const IconDiv = styled.div`
   width: 36px;
   height: 36px;
 `;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 60px;
+  gap: 20px;
+
+  margin-bottom: 30px;
+`;
+
+export const TrackButton = styled.button<{ $isSelected: boolean }>`
+  display: flex;
+  width: 220px;
+  padding: 18px 0;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: ${({ $isSelected }) =>
+    $isSelected
+      ? 'var(--FarmSystem_Green05, #248634)'
+      : 'var(--FarmSystem_Green04, #49AA59)'};
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  transition: transform 0.3s ease, background 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    background-color: var(--FarmSystem_Green05, #248634);
+  }
+`;
+
+
+export const TrackButtonText = styled.p`
+  color: var(--FarmSystem_White, #FCFCFC);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 40px; /* 200% */
+`; 
--- src/pages/Main/Tracks/Tracks.tsx
+++ src/pages/Main/Tracks/Tracks.tsx
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import * as S from './Tracks.styled';
 import { TracksData } from './TracksData';
 import InfoBox from './InfoBox';
 import OrangeArrow from '@/assets/Icons/OrangeArrow.svg';
 
 export default function Tracks() {
+  const [selectedTrack, setSelectedTrack] = useState(TracksData[0]);
 
   return (
     <S.Container>
       <S.CenterContainer>
-      <S.Title>트랙 및 커리큘럼</S.Title>
-      <S.GoToUnion>
-        <S.GoToUnionText>
-          트랙 선택이 고민된다면 <S.OrangeHighlight>Union</S.OrangeHighlight>으로!
-        </S.GoToUnionText>
-        <S.IconDiv>
-          <img src={OrangeArrow} alt="Go to Union" />
-        </S.IconDiv>
-      </S.GoToUnion>
-      <InfoBox />
+        <S.Title>트랙 및 커리큘럼</S.Title>
+        <S.GoToUnion>
+          <S.GoToUnionText>
+            트랙 선택이 고민된다면 <S.OrangeHighlight>Union</S.OrangeHighlight>으로!
+          </S.GoToUnionText>
+          <S.IconDiv>
+            <img src={OrangeArrow} alt="Go to Union" />
+          </S.IconDiv>
+        </S.GoToUnion>
+        <S.ButtonContainer>
+          {TracksData.map((track, index) => (
+            <S.TrackButton
+              key={index}
+              $isSelected={selectedTrack.name === track.name}
+              onClick={() => setSelectedTrack(track)}
+            >
+              <S.TrackButtonText>{track.name}</S.TrackButtonText>
+            </S.TrackButton>
+          ))}
+        </S.ButtonContainer>
+        <InfoBox />
       </S.CenterContainer>
     </S.Container>
   );
