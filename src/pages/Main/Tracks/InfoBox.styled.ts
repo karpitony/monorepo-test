540101740097d55028767e8be21efed80c011ec5
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-12 21:56:33 +0900

    feat: 트랙 별 커리큘럼 카드 추가

--- src/pages/Main/Tracks/InfoBox.styled.ts
+++ src/pages/Main/Tracks/InfoBox.styled.ts
@@ -42,3 +42,54 @@ export const TrackDescription = styled.p`
   font-weight: 500;
   line-height: 30px; /* 150% */
 `;
+
+export const PhasesContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+`;
+
+export const PhaseCard = styled.div`
+  display: flex;
+  width: 300px;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 11px;
+
+  border-radius: 20px;
+  background: var(--FarmSystem_White, #FCFCFC);
+`;
+
+export const PhaseTitle = styled.h3`
+  color: var(--FarmSystem_Green07, #175321);
+  text-align: center;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+
+  margin-bottom: 11px;
+`;
+
+export const PhaseContent = styled.ul`
+  display: flex;
+  width: 260px;
+  height: 170px;
+  flex-direction: column;
+  justify-content: center;
+
+  color: var(--FarmSystem_Black, #191919);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px; /* 150% */
+  list-style-type: disc;
+  list-style-position: inside;
+`;
+
+export const Li = styled.li`
+  padding-left: 10px;
+`;
--- src/pages/Main/Tracks/InfoBox.tsx
+++ src/pages/Main/Tracks/InfoBox.tsx
@@ -7,6 +7,8 @@ interface InfoBoxProps {
 }
 
 export default function InfoBox({ selectedTrack }: InfoBoxProps) {
+  const phaseLabels = ["1학기", "여름방학", "2학기"];
+
   return (
     <S.Container>
       <S.TrackNameContainer>
@@ -14,6 +16,18 @@ export default function InfoBox({ selectedTrack }: InfoBoxProps) {
         <S.TrackName>{selectedTrack.name}</S.TrackName>
       </S.TrackNameContainer>
       <S.TrackDescription>{selectedTrack.description}</S.TrackDescription>
+      <S.PhasesContainer>
+        {Object.entries(selectedTrack.phases).map(([phaseKey, phaseItems], index) => (
+          <S.PhaseCard key={phaseKey}>
+            <S.PhaseTitle>{phaseLabels[index]}</S.PhaseTitle>
+            <S.PhaseContent>
+              {phaseItems.map((item, i) => (
+                <S.Li key={`${phaseKey}-${i}`}>{item}</S.Li>
+              ))}
+            </S.PhaseContent>
+          </S.PhaseCard>
+        ))}
+      </S.PhasesContainer>
     </S.Container>
   );
 }
