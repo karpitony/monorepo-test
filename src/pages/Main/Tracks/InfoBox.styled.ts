e8daec00faa33f863964e86e74af0c9ae4b4d123
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-13 00:03:15 +0900

    feat: 교수님 관련 정보 추가

--- src/pages/Main/Tracks/InfoBox.styled.ts
+++ src/pages/Main/Tracks/InfoBox.styled.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 1170px;
-  height: 790px;
   background: var(--FarmSystem_LightGrey);
   border-radius: 10px;
   padding: 50px 60px;
@@ -48,6 +47,8 @@ export const PhasesContainer = styled.div`
   display: flex;
   justify-content: center;
   gap: 30px;
+
+  margin-bottom: 51px;
 `;
 
 export const PhaseCard = styled.div`
@@ -86,10 +87,87 @@ export const PhaseContent = styled.ul`
   font-style: normal;
   font-weight: 400;
   line-height: 30px; /* 150% */
-  list-style-type: disc;
-  list-style-position: inside;
 `;
 
 export const Li = styled.li`
-  padding-left: 10px;
+  
+`;
+
+export const ProfessorNameContainer = styled.div`
+  width: 100%;
+  height: 29px;
+
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 30px;
+
+  margin-bottom: 20px;
+`;
+
+export const ProfessorText = styled.p`
+  color: var(--FarmSystem_Green07, #175321);
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const ProfessorName = styled.p`
+  color: var(--FarmSystem_Black, #191919);
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 29px; /* 120.833% */
+`;
+
+
+export const ProfessorProfile = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 38px;
+`;
+
+export const ProfessorImage = styled.img`
+  width: 100px;
+  height: 100px;
+  flex-shrink: 0;
+  aspect-ratio: 1/1;
+`;
+
+export const ProfileList = styled.ul`
+  list-style-type: none;
+  list-style-position: inside;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 30px;
+  row-gap: 15px;
+`;
+
+export const ProfileListItem = styled.li`
+  gap: 30px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+
+export const ProfileListKey = styled.p`
+  color: var(--FarmSystem_Black, #191919);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px; /* 100% */
+`;
+
+export const ProfileListValue = styled.p`
+  width: 300px;
+  flex-shrink: 0;
+  color: var(--FarmSystem_Black, #191919);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 100% */
 `;
--- src/pages/Main/Tracks/InfoBox.tsx
+++ src/pages/Main/Tracks/InfoBox.tsx
@@ -11,23 +11,63 @@ export default function InfoBox({ selectedTrack }: InfoBoxProps) {
 
   return (
     <S.Container>
+
+      {/* 트랙 이름 + 소개개 */}
       <S.TrackNameContainer>
         <img src={RobotArm} alt="Robot Arm" style={{"width":"32px","height":"32px"}} />
         <S.TrackName>{selectedTrack.name}</S.TrackName>
       </S.TrackNameContainer>
       <S.TrackDescription>{selectedTrack.description}</S.TrackDescription>
+      
+      {/* 트랙 학기별 커리큘럼 */}
       <S.PhasesContainer>
         {Object.entries(selectedTrack.phases).map(([phaseKey, phaseItems], index) => (
           <S.PhaseCard key={phaseKey}>
             <S.PhaseTitle>{phaseLabels[index]}</S.PhaseTitle>
             <S.PhaseContent>
               {phaseItems.map((item, i) => (
-                <S.Li key={`${phaseKey}-${i}`}>{item}</S.Li>
+                <S.Li key={`${phaseKey}-${i}`}>&bull; {item}</S.Li>
               ))}
             </S.PhaseContent>
           </S.PhaseCard>
         ))}
       </S.PhasesContainer>
+
+      {/* 담당 교수 정보 */}
+      <S.ProfessorNameContainer>
+        <S.ProfessorText>담당 교수</S.ProfessorText>
+        <S.ProfessorName>{selectedTrack.professorData.name}</S.ProfessorName>
+      </S.ProfessorNameContainer>
+      <S.ProfessorProfile>
+        <S.ProfessorImage src={selectedTrack.professorData.image} alt="Professor" />
+      
+        <S.ProfileList>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 연락처</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.phone}</S.ProfileListValue>
+          </S.ProfileListItem>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 최종학력</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.education}</S.ProfileListValue>
+          </S.ProfileListItem>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 이메일</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.email}</S.ProfileListValue>
+          </S.ProfileListItem>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 전공분야</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.major}</S.ProfileListValue>
+          </S.ProfileListItem>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 연구실</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.location}</S.ProfileListValue>
+          </S.ProfileListItem>
+          <S.ProfileListItem>
+            <S.ProfileListKey>&bull; 연구분야</S.ProfileListKey>
+            <S.ProfileListValue>{selectedTrack.professorData.research}</S.ProfileListValue>
+          </S.ProfileListItem>
+        </S.ProfileList>
+      </S.ProfessorProfile>
     </S.Container>
   );
 }
