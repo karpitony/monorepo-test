c5649b02a19e04753d23199fb5185f51f380f33a
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-12 20:48:51 +0900

    feat: 유니온으로 이동 텍스트 추가

--- src/pages/Main/Tracks/Tracks.styled.ts
+++ src/pages/Main/Tracks/Tracks.styled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
+  width: 100%;
   height: 1244px;
   user-select: none;
 
@@ -8,4 +9,64 @@ export const Container = styled.section`
   flex-direction: column; 
   justify-content: center; 
   align-items: center; 
+`;
+
+export const CenterContainer = styled.div`
+  max-width: 1170px;
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center; 
+`;
+
+export const Title = styled.h2`
+  width: 100%;
+  padding-left: 5px;
+  margin-bottom: 70px;
+
+  color: var(--FarmSystem_Green01, #28723F);
+  text-align: left;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 700;
+
+  line-height: 57px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+export const GoToUnion = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  
+  width: 100%;
+  height: 36px;
+  cursor: pointer;
+
+  margin-bottom: 28px;
+`;
+
+export const GoToUnionText = styled.p`
+  color: var(--FarmSystem_Black, #191919);
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28px;
+  letter-spacing: -0.24px;
+`;
+
+export const OrangeHighlight = styled.span`
+  color: var(--FarmSystem_Orange);
+`;
+
+export const IconDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 36px;
+  height: 36px;
 `;
--- src/pages/Main/Tracks/Tracks.tsx
+++ src/pages/Main/Tracks/Tracks.tsx
@@ -1,12 +1,24 @@
 import * as S from './Tracks.styled';
 import { TracksData } from './TracksData';
 import InfoBox from './InfoBox';
+import OrangeArrow from '@/assets/Icons/OrangeArrow.svg';
 
 export default function Tracks() {
+
   return (
     <S.Container>
-      <h2>Tracks Section</h2>
+      <S.CenterContainer>
+      <S.Title>트랙 및 커리큘럼</S.Title>
+      <S.GoToUnion>
+        <S.GoToUnionText>
+          트랙 선택이 고민된다면 <S.OrangeHighlight>Union</S.OrangeHighlight>으로!
+        </S.GoToUnionText>
+        <S.IconDiv>
+          <img src={OrangeArrow} alt="Go to Union" />
+        </S.IconDiv>
+      </S.GoToUnion>
       <InfoBox />
+      </S.CenterContainer>
     </S.Container>
   );
 }
