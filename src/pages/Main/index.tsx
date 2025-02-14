0beb609c3ace5cd46b5ada0d22e7953852a6bc49
parkjoohyung <dochipjh@gmail.com>
2025-02-15 00:20:16 +0900

    feat: 백그라운드 블러 효과 적용

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,15 +3,18 @@ import Tracks from "./Tracks/Tracks";
 import Union from "./Union/Union";
 import Achievements from "./Achievements/Achievements";
 import BottomInfo from "./BottomInfo/BottomInfo";
+import FarmSystemNav from "./FarmSyetemNav/FarmSystemNav";
+import * as S from "./index.styled"; 
 
 export default function Main() {
   return (
-    <div>
+    <S.MainContainer> {/* 배경 그라데이션 적용 */}
+      <FarmSystemNav />
       <Intro />
       <Tracks />
       <Union />
       <Achievements />
       <BottomInfo />
-    </div>
+    </S.MainContainer>
   );
 }
