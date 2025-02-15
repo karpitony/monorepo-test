0c0a20c282d7276d3f97fad6cad9a3af948613e4
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-16 00:42:13 +0900

    feat: 모바일용 헤더 추가

--- src/pages/Main/FarmSyetemNav/FarmSystemNav.styled.ts
+++ src/pages/Main/FarmSyetemNav/FarmSystemNav.styled.ts
@@ -7,11 +7,13 @@ interface FixedNavProps {
 
 export const FixedNavWrapper = styled.div<FixedNavProps>`
   position: fixed;
-  top: ${({ isVisible }) => (isVisible ? "10px" : "-50px")};
+  top: ${({ isVisible, $isMobile }) =>
+    !$isMobile ? (isVisible ? "10px" : "-50px") :(isVisible ? "20px" : "-150px")};
   right: 20px;
   transition: top 0.3s ease-in-out;
   z-index: 1000;
 `;
+
 
 export const Navbar = styled.nav<{ $isMobile: boolean }>`
   background-color: #216D35;
@@ -42,4 +44,35 @@ export const Circle = styled.div<{ $isMobile: boolean }>`
   background-color: white;
   border-radius: 50%;
   margin-right: 6px;
+`;
+
+export const MobileNavbar = styled.div`
+  display: inline-flex;
+  height: 145px;
+  padding: 16px 19px 15px 19px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 20px 0px 20px 20px;
+  border: 1px solid var(--FarmSystem_White, #FCFCFC);
+  background: var(--FarmSystem_Green01, #28723F);
+`;
+
+export const MobileNavItem = styled.a`
+  display: flex;
+  padding: 5px 0px;
+  align-items: center;
+  gap: 5px;
+  flex: 1 0 0;
+`;
+
+export const MobileNavText = styled.p`
+  color: var(--FarmSystem_White, #FCFCFC);
+  text-align: center;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px; /* 160% */
+  letter-spacing: -0.24px;
 `;
--- src/pages/Main/FarmSyetemNav/FarmSystemNav.tsx
+++ src/pages/Main/FarmSyetemNav/FarmSystemNav.tsx
@@ -34,20 +34,37 @@ export default function FarmSystemNav() {
 
   return (
     <S.FixedNavWrapper isVisible={isVisible} $isMobile={isMobile}>
-      <S.Navbar $isMobile={isMobile}>
-        <S.NavItem $isMobile={isMobile} href="#about" onClick={(e) => handleSmoothScroll(e, "#about")}>
-          <S.Circle $isMobile={isMobile} /> Farm System이란?
-        </S.NavItem>
-        <S.NavItem $isMobile={isMobile} href="#tracks" onClick={(e) => handleSmoothScroll(e, "#tracks")}>
-          <S.Circle $isMobile={isMobile} /> 트랙 및 커리큘럼
-        </S.NavItem>
-        <S.NavItem $isMobile={isMobile} href="#achievements" onClick={(e) => handleSmoothScroll(e, "#achievements")}>
-          <S.Circle $isMobile={isMobile} /> 활동 및 성과
-        </S.NavItem>
-        <S.NavItem $isMobile={isMobile} href="#eligibility" onClick={(e) => handleSmoothScroll(e, "#eligibility")}>
-          <S.Circle $isMobile={isMobile} /> 지원 요건
-        </S.NavItem>
-      </S.Navbar>
+      {isMobile ? (
+        <S.MobileNavbar>
+          <S.MobileNavItem href="#about" onClick={(e) => handleSmoothScroll(e, "#about")}>
+            <S.MobileNavText> Farm System이란?</S.MobileNavText>
+          </S.MobileNavItem>
+          <S.MobileNavItem href="#tracks" onClick={(e) => handleSmoothScroll(e, "#tracks")}>
+            <S.MobileNavText> 트랙 및 커리큘럼</S.MobileNavText>
+          </S.MobileNavItem>
+          <S.MobileNavItem href="#achievements" onClick={(e) => handleSmoothScroll(e, "#achievements")}>
+            <S.MobileNavText> 활동 및 성과</S.MobileNavText>
+          </S.MobileNavItem>
+          <S.MobileNavItem href="#eligibility" onClick={(e) => handleSmoothScroll(e, "#eligibility")}>
+            <S.MobileNavText> 지원 요건</S.MobileNavText>
+          </S.MobileNavItem>
+        </S.MobileNavbar>
+      ) : (
+        <S.Navbar $isMobile={isMobile}>
+          <S.NavItem $isMobile={isMobile} href="#about" onClick={(e) => handleSmoothScroll(e, "#about")}>
+            <S.Circle $isMobile={isMobile} /> Farm System이란?
+          </S.NavItem>
+          <S.NavItem $isMobile={isMobile} href="#tracks" onClick={(e) => handleSmoothScroll(e, "#tracks")}>
+            <S.Circle $isMobile={isMobile} /> 트랙 및 커리큘럼
+          </S.NavItem>
+          <S.NavItem $isMobile={isMobile} href="#achievements" onClick={(e) => handleSmoothScroll(e, "#achievements")}>
+            <S.Circle $isMobile={isMobile} /> 활동 및 성과
+          </S.NavItem>
+          <S.NavItem $isMobile={isMobile} href="#eligibility" onClick={(e) => handleSmoothScroll(e, "#eligibility")}>
+            <S.Circle $isMobile={isMobile} /> 지원 요건
+          </S.NavItem>
+        </S.Navbar>
+      )}
     </S.FixedNavWrapper>
   );
 }
