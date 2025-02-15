4cd7ee671595d1b95e763ff7b342a3f7fe89d49b
parkjoohyung <dochipjh@gmail.com>
2025-02-16 03:19:40 +0900

    [Feat] 메인페이지 인트로 반응형 구현현

--- src/hooks/useMediaQueries.ts
+++ src/hooks/useMediaQueries.ts
@@ -5,16 +5,20 @@ const useMediaQueries = () => {
    * Union 작업중 콘텐츠 박스에서 767보다 더 작은 크기 조절이 필요해서 isTiny를 추가했습니다.
    * 반응형 작업하실 때 한번더 봐주시고 고쳐주시면 좋을거 같습니다. 
   */
-  const isTiny = useMediaQuery({ query: "(max-width: 385px)" })
+  const isTiny = useMediaQuery({ query: "(max-width: 385px)" });
+  const isApp = useMediaQuery({ query: "(max-width: 440px)"});
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const isTablet = useMediaQuery({
     query: "(min-width: 768px) and (max-width: 1023px)",
+  });
+  const isIpadPro = useMediaQuery({
+    query: "(min-width: 700px) and (max-width: 1400px)",
   });
   const isDesktop = useMediaQuery({
     query: "(min-width: 1024px)" 
   });
 
-  return { isTiny, isMobile, isTablet, isDesktop };
+  return { isTiny, isApp, isMobile, isTablet, isDesktop, isIpadPro };
 };
 
 export default useMediaQueries;
