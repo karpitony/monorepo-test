02611d76f63d935110c636dae4ef136184ef056b
parkjoohyung <dochipjh@gmail.com>
2025-02-23 21:28:36 +0900

    feat: FAQ 컨테니너바 반응형 수정

--- src/pages/FAQ/Frame.styled.ts
+++ src/pages/FAQ/Frame.styled.ts
@@ -1,56 +1,126 @@
 import styled from 'styled-components';
 
-export const FrameParent = styled.div`
+const media = {
+  mobile: '@media (max-width: 480px)',
+  tablet: '@media (max-width: 768px)',
+  app: '@media (max-width: 1024px)',
+};
+
+export const FrameParent = styled.div<{ $isApp: boolean; $isMobile: boolean; $isTablet: boolean }>`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  gap: 2vw;
+  gap: 20px;
   text-align: center;
-  font-size: 1.6vw;
   color: #757575;
+
+  font-size: 40px; 
+
+  ${({ $isMobile }) => $isMobile && `
+    font-size: 30px; 
+    flex-wrap: wrap;         
+    width: 350px;         
+    justify-content: center;
+  `}
+
+  ${({ $isTablet }) => $isTablet && `
+    font-size: 35px; 
+  `}
+
+  ${({ $isApp }) => $isApp && `
+    gap: 10px;
+    font-size: 40px; 
+    flex-wrap: wrap;         
+    width: 300px;         
+    justify-content: center;
+  `}
 `;
 
-export const Container = styled.button<{ isSelected: boolean }>`
+export const Container = styled.button<{ isSelected: boolean, $isApp: boolean; $isMobile: boolean; $isTablet: boolean }>`
   border-radius: 10px;
   background-color: ${({ isSelected }) => (isSelected ? '#006811' : '#f1f1f1')};
   color: ${({ isSelected }) => (isSelected ? '#ffffff' : '#757575')};
-  height: 7vh;
   overflow: hidden;
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding: 0.5vw 2vw;
   box-sizing: border-box;
   cursor: pointer;
   border: none;
-  font-size: 1vw;
   font-weight: 550;
   letter-spacing: -0.24px;
+
+  height: 33px;
+  padding: 5px 20px;
+
   &:hover {
     background-color: #006811;
     color: #ffffff;
   }
+
+  ${({ $isMobile }) => $isMobile && `
+     height: 33px;
+     padding: 5px 20px;
+  `}
+
+  ${({ $isTablet }) => $isTablet && `
+     height: 33px;
+     padding: 5px 20px;
+  `}
+
+  ${({ $isApp }) => $isApp && `
+    height: 33px;
+    padding: 5px 15px;
+  `}
 `;
 
 export const Div = styled.div`
   letter-spacing: -0.24px;
   line-height: 2vh;
   font-weight: 600;
+
+  ${media.mobile} {
+    line-height: 1.5vh;
+    font-size: 3vw;
+  }
+
+  ${media.tablet} {
+    line-height: 1.8vh;
+    font-size: 2vw;
+  }
+
+  ${media.app} {
+    line-height: 2vh;
+    font-size: 1.8vw;
+  }
 `;
 
 export const Button = styled.button`
   background: none;
   border: none;
-  font-size: 1.6vw;
   font-weight: 600;
   letter-spacing: -0.24px;
   line-height: 2vh;
   color: #757575;
   cursor: pointer;
+
+  font-size: 1.6vw;
+
   &:hover {
     background-color: #006811;
     color: #ffffff;
+  }
+
+  ${media.mobile} {
+    font-size: 4vw;
+  }
+
+  ${media.tablet} {
+    font-size: 2.5vw;
+  }
+
+  ${media.app} {
+    font-size: 2vw;
   }
 `;
