5c96550597507cf96c447e8bd0414524f661320f
dongmin0204 <nm2205@dgu.ac.kr>
2025-02-25 21:22:48 +0900

    feat: 오픈 전 페이지 제작 커밋

---
+++ src/pages/MaintainPage/MaintainPage.tsx
@@ -0,0 +1,22 @@
+import * as S from "./MaintainPage.styled"
+import useMediaQueries from "@/hooks/useMediaQueries";
+
+
+
+export default function MaintainPage() {
+    const { isMobile } = useMediaQueries();
+    return (
+        <S.Container>
+            <S.Title $isMobile = {isMobile}>
+                Coming Soon 
+            </S.Title>
+            <S.SubTitle $isMobile = {isMobile}>
+              ✨ 3월 1일 오픈 예정 ✨
+            </S.SubTitle>
+            <S.Description $isMobile = {isMobile}>
+              곧 팜시스템의 홈페이지를 만날 수 있어요! 조금만 기다려 주세요😉
+            </S.Description>
+        </S.Container>
+    )
+};
+
