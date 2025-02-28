04870173a79e51656e9647e5bfffe91007a8b015
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-02-28 17:24:31 +0900

    feat: 지원서 박스 페이지와 연결

--- src/pages/Apply/index.tsx
+++ src/pages/Apply/index.tsx
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import ApplyBox from './ApplyBox';
+
 export default function Apply() {
+  const [choice, setChoice] = useState<" " | "새 지원서" | "불러오기">(" ");
   return (
-    <div>
+    <ApplyContainer>
       <h1>Apply Page </h1>
-    </div>
+      {choice === " " ?
+        <ButtonContainer>
+          <h2>지원서 작성 방법을 선택해주세요</h2>
+          <button onClick={() => setChoice("새 지원서")}>새 지원서 작성</button>
+          <button onClick={() => setChoice("불러오기")}>지원서 불러오기</button>
+        </ButtonContainer>
+        :
+        <>
+          <button onClick={() => setChoice(" ")}>돌아가기</button>
+          <ApplyBox isFirst={choice === "새 지원서"} />
+        </>
+      }
+    </ApplyContainer>
   );
 }
+
+const ApplyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 100px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
