7e4f74e318edf2ecbd5abf37528b41580c6d9b0b
dewbeeny <dewbeeny@gmail.com>
2025-03-01 03:23:05 +0900

    fix: 소식 페이지 스타일 블로그/프로젝트와 일치

--- src/pages/News/News.styles.ts
+++ src/pages/News/News.styles.ts
@@ -8,8 +8,27 @@ export const Container = styled.div`
   text-align: center;
 `;
 
-export const Message = styled.p`
-  font-size: 1.5rem;
-  color: #333;
+export const DescriptionContainer = styled.div`
+  width: 100%;
+  margin: 20px;
+  display: block;
+`;
+
+export const Message = styled.div<{$isMobile: boolean;}>`
+  padding-top: 20vh;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  flex-direction: column;
+  color: black;
+  font-size: ${(props) => (props.$isMobile ? "20px" : "32px")};
   font-weight: 600;
+  margin-bottom: 35px; 
+
+  a {
+    font-size: ${(props) => (props.$isMobile ? "10px" : "14px")};
+    font-weight: 300;
+    margin-top: 10px; 
+  }
 `;
