4aa4f6cb8fb0e84abaae95bbf8223575881818b4
dewbeeny <dewbeeny@gmail.com>
2025-03-01 03:07:51 +0900

    fix: 트랙, 유니온 소개 내용 변경 및 아이콘 png 파일 추가

--- src/pages/Main/Tracks/InfoBox.styled.ts
+++ src/pages/Main/Tracks/InfoBox.styled.ts
@@ -80,18 +80,15 @@ export const PhaseTitle = styled.h3<{ $isMobile: boolean }>`
 export const PhaseContent = styled.ul<{ $isMobile: boolean }>`
   display: flex;
   width: 100%;
-  height: ${({ $isMobile }) => ($isMobile ? "150px" : "170px")};
+  height: ${({ $isMobile }) => ($isMobile ? "180px" : "200px")};
   flex-direction: column;
-  justify-content: left;
-  text-align: left;
-  font-size: ${({ $isMobile }) => ($isMobile ? "14px" : "18px")};
+  justify-content: flex-start;
+  text-align: center;
+  font-size: ${({ $isMobile }) => ($isMobile ? "13px" : "18px")};
   color: var(--FarmSystem_Black, #191919);
   font-style: normal;
   font-weight: 400;
   line-height: 30px; /* 150% */
-
-  padding-left: 20px;
-
 `;
 
 export const Li = styled.li<{ $isMobile: boolean }>`
