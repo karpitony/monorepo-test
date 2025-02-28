8b487c03d24340eb102e7bc8bda14fcc1a3968d3
dewbeeny <dewbeeny@gmail.com>
2025-02-28 17:51:04 +0900

    feat: 소식 페이지 생성 및 등록된 글이 없을 때 안내 메시지 추가

---
+++ src/pages/News/News.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+`;
+
+export const Message = styled.p`
+  font-size: 1.5rem;
+  color: #333;
+  font-weight: 600;
+`;
