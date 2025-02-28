c17ed5c1c9815d2a923ec9e7296fd75a2c5c0b81
Yunseok <watemark1017@gmail.com>
2025-02-28 22:58:19 +0900

    fix: apiConfig 좀더 유연하게 동작하게

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -3,18 +3,22 @@ import axios from 'axios';
 const getApiBaseUrl = () => {
   // 개발 모드이거나(로컬호스트등 vite가 알아서 인식) dev.로 시작하는 도메인이면 개발 서버 주소로 설정
   const hostname = window.location.hostname;
+  let baseUrl;
 
   if (import.meta.env.MODE === 'development' || hostname.startsWith('dev.')) {
-    return import.meta.env.VITE_BASE_DEV_URL; // 개발 서버
+    baseUrl = import.meta.env.VITE_BASE_DEV_URL; // 개발 서버
   } else {
-    return import.meta.env.VITE_BASE_URL; // 운영 서버
+    baseUrl = import.meta.env.VITE_BASE_URL; // 운영 서버
   }
+  
+  // URL이 '/'로 끝나면 'api/', 아니면 '/api/'
+  return baseUrl.endsWith('/') ? `${baseUrl}api/` : `${baseUrl}/api/`;
 };
 
 const API_BASE_URL = getApiBaseUrl();
 
 const apiConfig = axios.create({
-  baseURL: `${API_BASE_URL}api/`,
+  baseURL: API_BASE_URL,
   timeout: 5000, // 요청 타임아웃 5초
   headers: {
     'Content-Type': 'application/json',
